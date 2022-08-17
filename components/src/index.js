445ac6a6f2547f32f84c09e0e4a42ab3a4a0f4bf
JakubMizera <kubamiz53@gmail.com>
2022-08-17 17:38:06 +0200

    examples of using components and props

--- components/src/index.js
+++ components/src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from "faker";
+
+import CommentDetail from './CommentDetail';
 
 const App = () => {
-  return <div>test</div>;
+  return (
+    <div className='ui container comments'>
+      <CommentDetail
+        author="Sam"
+        time="Today at 6:00PM"
+        text="Nice blog post"
+        picture={faker.image.image()}
+      />
+      <CommentDetail
+        author="Jane"
+        time="Today at 5:36PM"
+        text="Nice comment bro"
+        picture={faker.image.image()}
+      />
+      <CommentDetail
+        author="Alex"
+        time="Yesterday at 11:53PM"
+        text="I don't know what I'm doing"
+        picture={faker.image.image()}
+      />
+    </div>
+  );
 };
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));

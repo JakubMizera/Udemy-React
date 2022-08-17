2f846fb16f4746398b1d66c936db03ec8eb01d42
JakubMizera <kubamiz53@gmail.com>
2022-08-17 12:54:14 +0200

    deleted unneccessary files

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 1) import React and ReactDOM libraries
+import React from 'react' // Library that defines what a component is and how they work together
+import ReactDOM from 'react-dom/client' //Library that knows how to show a component in the browser
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2) Get a refrence to the div with ID root
+const el = document.getElementById('root');
+console.log(el)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// 4) Create a component
+const App = () => {
+    return <h1>Hello there!</h1>;
+}
+
+// 5) Show component on a screen
+root.render(<App />);

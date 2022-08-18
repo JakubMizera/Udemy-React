114e2f5cb9e658edffd4e018bd8fc8c570bf61b7
JakubMizera <kubamiz53@gmail.com>
2022-08-18 13:26:37 +0200

    geolocation position from api

--- seasons-app/src/index.js
+++ seasons-app/src/index.js
@@ -4,8 +4,14 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (error) => console.log(error)
+  );
+
+
   return (
-    <div></div>
+    <div>Latitude: </div>
   )
 }
 

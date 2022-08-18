cf2bb0991b6777e4d65c39c7de757bb0c8975a28
JakubMizera <kubamiz53@gmail.com>
2022-08-18 12:05:59 +0200

    send children thru props, additional styling

--- components/src/index.js
+++ components/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import faker from "faker";
 
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import App from './style/App.css'
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
@@ -10,26 +12,38 @@ const root = ReactDOM.createRoot(el);
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail
-        author="Sam"
-        time="Today at 6:00PM"
-        text="Nice blog post"
-        picture={faker.image.image()}
-      />
-      <CommentDetail
-        author="Jane"
-        time="Today at 5:36PM"
-        text="Nice comment bro"
-        picture={faker.image.image()}
-      />
-      <CommentDetail
-        author="Alex"
-        time="Yesterday at 11:53PM"
-        text="I don't know what I'm doing"
-        picture={faker.image.image()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          time="Today at 6:00PM"
+          text="Nice blog post"
+          picture={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          time="Today at 5:36PM"
+          text="Nice comment bro"
+          picture={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          time="Yesterday at 11:53PM"
+          text="I don't know what I'm doing"
+          picture={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
 
-root.render(<App/>);
+root.render(<App />);

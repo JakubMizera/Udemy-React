5e1d46cdf2c4bacc17f12ec4a567e3159936b89c
JakubMizera <kubamiz53@gmail.com>
2022-08-18 12:59:33 +0200

    app init files

---
+++ seasons-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const App = () => {
+  return (
+    <div></div>
+  )
+}
+
+root.render(<App />);

d4125670854836f76f234ba5d75c9ad1c8fac3b5
JakubMizera <kubamiz53@gmail.com>
2022-08-18 15:52:22 +0200

    changed App to React Class Component; constructor and setState

--- seasons-app/src/index.js
+++ seasons-app/src/index.js
@@ -3,16 +3,53 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (error) => console.log(error)
-  );
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (error) => console.log(error)
+//   );
 
 
-  return (
-    <div>Latitude: </div>
-  )
+//   return (
+//     <div>Latitude: </div>
+//   )
+// }
+
+class App extends React.Component {
+  //constructor is going to be called anytime an instance of this class is created
+  constructor(props) {
+    // super(props) is required everytime for constructor
+    super(props);
+
+    //this is the only time we do direct assignment to this.state
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position)
+        // !to update you need to use setState!
+        this.setState({ lat: position.coords.latitude });
+      },
+      (error) => {
+        console.log(error)
+        this.setState({ errorMessage: error.message })
+      }
+    );
+  }
+
+
+  // !React requires to define render in Class based components!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading, please wait</div>
+  }
 }
 
 root.render(<App />);

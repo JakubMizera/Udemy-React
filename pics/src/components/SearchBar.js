b4684eef0f2baf7f5b1a0d210eae392c3e704076
JakubMizera <kubamiz53@gmail.com>
2022-09-19 16:34:45 +0200

    refratoring from uncontrolled element to controlled

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value);
-    };
-    
+    state = { term: '' };
+
     render() {
         return (
             <div className='ui segment'>
                 <form className='ui form'>
                     <div className='field'>
                         <label>Image search</label>
-                        <input type='text' onChange={this.onInputChange}></input>
+                        <input
+                            type='text'
+                            value={this.state.term}
+                            onChange={(e) => this.setState({ term: e.target.value })}>
+                        </input>
                     </div>
                 </form>
             </div>

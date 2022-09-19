1685cd75bfcb307503705b069006fced445fdb45
JakubMizera <kubamiz53@gmail.com>
2022-09-19 16:26:09 +0200

    input change handler

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    onInputChange(event) {
+        console.log(event.target.value);
+    };
+    
     render() {
         return (
             <div className='ui segment'>
                 <form className='ui form'>
                     <div className='field'>
                         <label>Image search</label>
-                        <input type='text'></input>
+                        <input type='text' onChange={this.onInputChange}></input>
                     </div>
                 </form>
             </div>
         );
-    }
-}
+    };
+};
 
 export default SearchBar;

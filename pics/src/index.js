6316484fa42718bf24076d3af67aee92d817ceea
JakubMizera <kubamiz53@gmail.com>
2022-09-19 16:03:58 +0200

    init files

---
+++ pics/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+
+
+
+
+
+
+
+root.render(<App />);

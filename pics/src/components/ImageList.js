ac88eb676310c02bb06a4151c54e416f39eb4139
JakubMizera <kubamiz53@gmail.com>
2022-09-21 13:42:30 +0200

    ImageCard component + css styling

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,13 +1,15 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
     //map (({id, urls, description}) => ) instead for map((image) => )
 
-    let pictures = props.images.map(({ id, urls, description }) => {
-        return <img key={id} src={urls.regular} alt={description} />
+    let pictures = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image} />
     });
 
-    return <div>{pictures}</div>
+    return <div className='image-list'>{pictures}</div>
 };
 
 export default ImageList;

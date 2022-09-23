0c013dac222538bccc8262bc7d8c0f746d5f6471
JakubMizera <kubamiz53@gmail.com>
2022-09-23 16:09:59 +0200

    connected to yt API

--- youtube-api-browser/src/components/App.js
+++ youtube-api-browser/src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import SearchBar from './SearchBar';
+import youtube from '../APIs/youtube';
 
 class App extends React.Component {
+    state = { videos: [] };
 
-    onSearchSubmit = (searchInput) => {
-        console.log(searchInput)
+    onSearchSubmit = async (searchInput) => {
+        //console.log(searchInput);
+        const response = await youtube.get('/search', {
+            params: {
+                q: searchInput
+            }
+        });
+        this.setState({ videos: response.data.items })
+        //console.log(response.data.items[0].id.videoId);
     };
 
     render() {

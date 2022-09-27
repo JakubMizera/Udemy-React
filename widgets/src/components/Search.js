0a025d7ebef2d4470d5b48d8498aa2e353853b39
JakubMizera <kubamiz53@gmail.com>
2022-09-27 16:23:40 +0200

    useEffect examples

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,8 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Search = () => {
+    const [term, setTerm] = useState('');
+
+    console.log('I run with every render');
+    useEffect(() => {
+        console.log('I run after every render');
+    }, [term]);
+    //!useEffect 2nd argument can only be: [], 'empty', [some data]!
+    // [] => Run at initial render
+    // 'empty' => Run at initial render and after every rerender
+    // [data] => Run at initial render and after every rerender if data has changed since last render
+
     return (
-        <div>search</div>
+        <div>
+            <div className='ui form'>
+                <div className='field'>
+                    <label>Enter search term</label>
+                    <input
+                        className='input'
+                        value={term}
+                        onChange={(e) => setTerm(e.target.value)}
+                    ></input>
+                </div>
+            </div>
+        </div>
     );
 };
 

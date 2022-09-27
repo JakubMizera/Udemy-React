9b507e36f65ab4b932985689240931d6b410cd17
JakubMizera <kubamiz53@gmail.com>
2022-09-27 17:21:58 +0200

    how to use async func inside useEffect hook

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
-    const [term2, setTerm2] = useState('');
 
-    console.log('I run with every render');
     useEffect(() => {
-        console.log('I run after every render');
-    }, [term2]);
+        const searchWiki = async () => {
+            await axios.get('someAPI');
+        }
+        searchWiki();
+    }, [term]);
     //!useEffect 2nd argument can only be: [], 'empty', [some data]!
     // [] => Run at initial render
     // 'empty' => Run at initial render and after every rerender

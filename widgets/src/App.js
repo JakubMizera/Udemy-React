dcda3e85fba5e000a0008a90d17f90ad603bb60b
JakubMizera <kubamiz53@gmail.com>
2022-10-12 13:36:08 +0200

    Dropdown widget basic functionality

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,27 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import { items, options } from './content/content';
 
-const items = [
-    {
-        title: 'What is React?',
-        content: 'React is a front end javascript framework'
-    },
-    {
-        title: 'Why use React?',
-        content: 'React is a favorite JS library among engineers'
-    },
-    {
-        title: 'How do you use React?',
-        content: 'You use React by creating components'
-    }
-]
 
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div>
             {/* <Accordion items={items} /> */}
-            <Search />
+            {/* <Search /> */}
+            <Dropdown
+                selected={selected}
+                onSelectedChange={setSelected}
+                options={options}
+            />
         </div>
     );
 };
---
+++ widgets/src/content/content.js
@@ -0,0 +1,29 @@
+export const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+]
+
+export const options = [
+    {
+        label: 'The color red',
+        value: 'red',
+    },
+    {
+        label: 'The color green',
+        value: 'green',
+    },
+    {
+        label: 'A shade of blue',
+        value: 'blue',
+    },
+]

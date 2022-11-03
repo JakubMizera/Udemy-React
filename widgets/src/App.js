0b4bf8757ef5b90063512ad53847f86494787520
JakubMizera <kubamiz53@gmail.com>
2022-11-03 16:35:12 +0100

    react route nav for widgets

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { items, options } from './content/content';
+
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
-import { items, options } from './content/content';
 
 
 export default () => {
@@ -11,19 +13,31 @@ export default () => {
     const [showDropdown, setShowDropdown] = useState(true);
 
     return (
-        <div>
-            {/* <Accordion items={items} /> */}
-            {/* <Search /> */}
-            {/* <button onClick={() => setShowDropdown(!showDropdown)}>Toggle button</button>
-            {showDropdown ?
-                <Dropdown
-                    selected={selected}
-                    onSelectedChange={setSelected}
-                    options={options}
-                    label='Select a color'
-                /> : null
-            } */}
-            <Translate />
-        </div>
+        <Router>
+            <>
+                <Link to='/'>Accordion</Link>
+                <Link to='/search'>Search</Link>
+                <Link to='/dropdown'>Dropdown</Link>
+                <Link to='/translate'>Translate</Link>
+            </>
+            <Routes>
+                <Route index element={<Accordion items={items} />} />
+                <Route path='search' element={<Search />} />
+                <Route path='dropdown' element={
+                    <>
+                        <button onClick={() => setShowDropdown(!showDropdown)}>Toggle button</button>
+                        {showDropdown ?
+                            <Dropdown
+                                selected={selected}
+                                onSelectedChange={setSelected}
+                                options={options}
+                                label='Select a color'
+                            /> : null
+                        }
+                    </>
+                } />
+                <Route path='translate' element={<Translate />} />
+            </Routes>
+        </Router>
     );
 };

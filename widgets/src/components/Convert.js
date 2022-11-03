409651cd768f88b52c2e9e6c0151720dece51dfa
JakubMizera <kubamiz53@gmail.com>
2022-11-03 15:21:49 +0100

    added debounced hook to limit APIs requests

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -4,12 +4,23 @@ import axios from "axios";
 
 const Convert = ({ language, text }) => {
     const [translated, setTranslated] = useState('');
+    const [debouncedText, setDebouncedText] = useState(text);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        }, 500);
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [text]);
 
     useEffect(() => {
         const doTranslation = async () => {
             const response = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
                 params: {
-                    q: text,
+                    q: debouncedText,
                     target: language.value,
                     key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
                 }
@@ -18,7 +29,7 @@ const Convert = ({ language, text }) => {
             setTranslated(response.data.data.translations[0].translatedText);
         };
         doTranslation();
-    }, [{ language, text }]);
+    }, [language, debouncedText]);
     //everytime language or text changes useEffect will rerender
     return (
         <div>

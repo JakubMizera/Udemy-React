5986494192f091c4e9e9b5b3544410d276c3c64a
JakubMizera <kubamiz53@gmail.com>
2022-11-16 15:05:40 +0100

    posts reducer

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -5,13 +5,16 @@ import { fetchPost } from '../actions';
 const PostList = (props) => {
     useEffect(() => {
         props.fetchPost();
-    });
+    }, []);
 
-
+    console.log(props.posts);
     return (
         <div>Post list</div>
     );
 };
 
+const mapStateToProps = (state) => {
+    return { posts: state.posts }
+};
 
-export default connect(null, { fetchPost: fetchPost })(PostList);
+export default connect(mapStateToProps, { fetchPost: fetchPost })(PostList);
